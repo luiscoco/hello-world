1b89829da94231e84625c3f8fe8559728e1150b1
VOpolskiy <vadopolski@gmail.com>
2023-09-28 13:16:02 +0200

    Added files

---
+++ src/main/scala/CountriesIssue.scala
@@ -0,0 +1,13 @@
+object CountriesIssue extends App {
+
+  /**
+  *
+   * Read json file with countries and select 10 countries with the biggest area in africa region
+   *
+   * Json can not contain the field "area" for some countries
+   *
+   *
+   *
+  * */
+
+}
